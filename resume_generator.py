a7e3d031948a35a443833818c8e3964854733fd1
Trevor <trevortomlin2@outlook.com>
2022-01-21 17:50:21 -0800

    Text is correctly parsed from csv.

--- resume_generator.py
+++ resume_generator.py
@@ -1,9 +1,31 @@
 import torch
 
-x = torch.rand(5, 3)
-print(x)
+# x = torch.rand(5, 3)
+# print(x)
 
-y = torch.rand(5, 3)
-print(y)
+# y = torch.rand(5, 3)
+# print(y)
 
-print(x+y)
+# print(x+y)
+
+import numpy as np
+import pandas as pd
+
+RESUME_PATH = 'data/Resume/Resume.csv'
+
+def get_text_from_csv(path):
+
+	df = pd.read_csv(path, sep=',', dtype=str, encoding='utf-8', index_col=False)
+
+	df = df['Resume_str']
+
+	return df.values.tolist()
+
+def main():
+	
+	text = get_text_from_csv(RESUME_PATH)
+
+	#print(text[0])
+
+if __name__ == '__main__':
+	main()
